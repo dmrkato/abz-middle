eebf75e5bf313f3552bbdc135ac4a7cd85208489
eJ <edward.lelickov@gmail.com>
2024-11-04 20:35:09 +0200

    Change faker locale

--- laravel/database/factories/PositionFactory.php
+++ laravel/database/factories/PositionFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Factory as FakerFactory;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Position>
@@ -16,8 +17,9 @@ class PositionFactory extends Factory
      */
     public function definition(): array
     {
+        $faker = FakerFactory::create('uk_UA');
         return [
-            'name' => fake()->jobTitle()
+            'name' => $faker->jobTitle()
         ];
     }
 }
--- laravel/database/factories/UserFactory.php
+++ laravel/database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use Faker\Factory as FakerFactory;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -23,10 +24,12 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $faker = FakerFactory::create('uk_UA');
+
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'phone' => fake()->e164PhoneNumber(),
+            'name' => $faker->name(),
+            'email' => $faker->unique()->safeEmail(),
+            'phone' => $faker->e164PhoneNumber(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
