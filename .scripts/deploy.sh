82ddb4491f4512dca21683ab53db537965259565
dmrkato <edward.lelickov@gmail.com>
2025-04-21 14:56:51 +0300

    github actions test

---
+++ .scripts/deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Exit on error on some step
+set -e
+
+# Move to app directory
+cd /home/ubuntu/abz-middle
+
+echo "Pulling latest changes from the repository..."
+git pull origin $(git rev-parse --abbrev-ref HEAD)
+
+#echo "Setting up environment variables..."
+#cp .env.example .env
+
+echo "Installing dependencies using Composer..."
+composer install --no-dev --optimize-autoloader
+
+#echo "Generating application key..."
+#php artisan key:generate
+
+echo "Running database migrations..."
+php artisan migrate
+
+echo "Caching configurations..."
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+echo "Deployment completed successfully!"
